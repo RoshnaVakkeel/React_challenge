041c6e7704aef835bb173bb31f5345fbbb721503
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-30 14:35:05 +0000

    Ädding content challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
+// import NavBarSimple from './components/NavBarSimple';
 import Sidebar from "./components/Sidebar"; 
+import NavBarForm from "./components/NavBarForm";
+import Content from './components/Content';
+import PostItem from './components/PostItem';
 
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple />
+      {/* <NavBarSimple /> */}
+      <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
